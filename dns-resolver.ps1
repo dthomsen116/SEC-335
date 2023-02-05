c3c8e53052e30c1b1900cde0f60b1f2605d60fcc
dthomsen116 <71229951+dthomsen116@users.noreply.github.com>
2023-02-05 15:53:10 -0500

    Dns-Resolvers

---
+++ dns-resolver.ps1
@@ -0,0 +1,10 @@
+﻿param($network, $dns)
+
+$network =
+
+$hostid = 0..255 | ForEach-Object {
+
+
+    Read-Host 'Resolve-DnsName -DnsOnly "$network.$hostid" -Server [Int]$dns' 
+
+    }
---
+++ dns-resolver2.ps1
@@ -0,0 +1,12 @@
+﻿param($network, $dns)
+
+$hostid = 0..255
+
+
+foreach($id in $hostid){
+
+Resolve-DnsName -Name "$network.$id" -Server "$dns" -ErrorAction SilentlyContinue | Select-Object Name, NameHost 
+
+
+
+}
